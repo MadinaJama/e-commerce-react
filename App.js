ea00a37507c4c86db059f8a133f2eebfa2d1caf6
Madina <madinajama9@gmail.com>
2023-01-27 08:21:11 -0500

    newly added js

---
+++ App.js
@@ -0,0 +1,19 @@
+import './index.css'
+import Nav from './components/nav.jsx';
+import Landing from './components/Landing'
+import Highlights from './components/Highlights';
+import Featured from './components/Featured';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <Nav />
+    <Landing />
+    <Highlights />
+    <Featured />
+    </div>
+  );
+}
+
+export default App;
